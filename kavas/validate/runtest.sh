59f52938fc28ca83ca8da9b5db6070abbe3ca055
yxjsolid <yxjsolid@gmail.com>
2015-03-31 00:31:05 +0800

    update

---
+++ kavas/validate/runtest.sh
@@ -0,0 +1,8 @@
+#./test ../vector/KASValidityTest_ECCEphemeralUnified_KDFConcat_NOKC_init.fax out
+#./test ../vector/KASValidityTest_ECCFullUnified_KDFConcat_NOKC_init.fax out
+#./test ../vector/KASValidityTest_ECCOnePassDH_KDFConcat_NOKC_init.fax out
+#./test ../vector/KASValidityTest_ECCOnePassUnified_KDFConcat_NOKC_init.fax out
+./test ../vector/KASValidityTest_ECCEphemeralUnified_KDFConcat_NOKC_resp.fax out
+#./test ../vector/KASValidityTest_ECCFullUnified_KDFConcat_NOKC_resp.fax out
+#./test ../vector/KASValidityTest_ECCOnePassDH_KDFConcat_NOKC_resp.fax out
+#./test ../vector/KASValidityTest_ECCOnePassUnified_KDFConcat_NOKC_resp.fax out
