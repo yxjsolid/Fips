5cfb45804c6587378d6942f28c0278040fde1348
Jerry Yang <xyang@sonicwall.com>
2015-04-01 20:08:47 +0800

    update

---
+++ kavas/validate/runNewGenTest.sh
@@ -0,0 +1,8 @@
+./test ../newGen/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_init.req ../newGen/resp/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_init.resp
+./test ../newGen/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_init.req ../newGen/resp/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_init.resp
+./test ../newGen/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_init.req ../newGen/resp/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_init.resp
+./test ../newGen/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_init.req ../newGen/resp/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_init.resp
+./test ../newGen/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_resp.req ../newGen/resp/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_resp.resp
+./test ../newGen/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_resp.req ../newGen/resp/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_resp.resp
+./test ../newGen/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_resp.req  ../newGen/resp/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_resp.resp
+./test ../newGen/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_resp.req ../newGen/resp/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_resp.resp
