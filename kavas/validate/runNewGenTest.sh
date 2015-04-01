42a0c6d8218a3bbf156370766de0f5c2276c4b3a
yxjsolid <yxjsolid@gmail.com>
2015-04-01 21:25:13 +0800

    update

--- kavas/validate/runNewGenTest.sh
+++ kavas/validate/runNewGenTest.sh
@@ -1,9 +1,9 @@
 ./test showError ../newGen/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_init.req ../newGen/resp/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_init.resp
-./test showError  ../newGen/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_resp.req ../newGen/resp/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_resp.resp
-./test showError  ../newGen/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_init.req      ../newGen/resp/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_init.resp     
-./test showError  ../newGen/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_resp.req      ../newGen/resp/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_resp.resp     
-./test showError  ../newGen/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_init.req        ../newGen/resp/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_init.resp       
-./test showError  ../newGen/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_resp.req        ../newGen/resp/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_resp.resp       
-./test showError  ../newGen/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_init.req   ../newGen/resp/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_init.resp  
-./test showError  ../newGen/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_resp.req   ../newGen/resp/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_resp.resp  
+./test showError ../newGen/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_resp.req ../newGen/resp/KASFunctionTest_ECCEphemeralUnified_KDFConcat_NOKC_resp.resp
+./test showError ../newGen/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_init.req ../newGen/resp/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_init.resp
+./test showError ../newGen/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_resp.req ../newGen/resp/KASFunctionTest_ECCFullUnified_KDFConcat_NOKC_resp.resp
+./test showError ../newGen/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_init.req ../newGen/resp/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_init.resp
+./test showError ../newGen/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_resp.req ../newGen/resp/KASFunctionTest_ECCOnePassDH_KDFConcat_NOKC_resp.resp
+./test showError ../newGen/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_init.req ../newGen/resp/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_init.resp
+./test showError ../newGen/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_resp.req ../newGen/resp/KASFunctionTest_ECCOnePassUnified_KDFConcat_NOKC_resp.resp
 
--- kavas/validate/runtest.sh
+++ kavas/validate/runtest.sh
@@ -6,7 +6,6 @@
 ./test quiet ../official/KASValidityTest_ECCFullUnified_KDFConcat_NOKC_resp.fax out
 ./test quiet ../official/KASValidityTest_ECCOnePassDH_KDFConcat_NOKC_resp.fax out
 ./test quiet ../official/KASValidityTest_ECCOnePassUnified_KDFConcat_NOKC_resp.fax out
-
 ./test quiet ../snwl/KASValidityTest_ECCEphemeralUnified_KDFConcat_NOKC_init.req ../snwl/resp/KASValidityTest_ECCEphemeralUnified_KDFConcat_NOKC_init.resp
 ./test quiet ../snwl/KASValidityTest_ECCFullUnified_KDFConcat_NOKC_init.req ../snwl/resp/KASValidityTest_ECCFullUnified_KDFConcat_NOKC_init.resp
 ./test quiet ../snwl/KASValidityTest_ECCOnePassDH_KDFConcat_NOKC_init.req ../snwl/resp/KASValidityTest_ECCOnePassDH_KDFConcat_NOKC_init.resp
